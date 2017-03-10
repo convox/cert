1177c958b6eba3caa2a183316cd4af6be3cae033
David Dollar <david@dollar.io>
2017-03-09 18:12:30 -0600

    self-signed certs

---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/big"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		usage()
+	}
+
+	if err := generate(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Println("usage: certs <domain> <cert> <key>")
+	os.Exit(1)
+}
+
+func generate() error {
+	cert, key, err := generateSelfSignedCertificate(os.Args[1])
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(os.Args[2], cert, 0644); err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(os.Args[3], key, 0600); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generateSelfSignedCertificate(host string) ([]byte, []byte, error) {
+	rkey, err := rsa.GenerateKey(rand.Reader, 2048)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	serial, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serial,
+		Subject: pkix.Name{
+			CommonName:   host,
+			Organization: []string{"convox"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		DNSNames:              []string{host},
+	}
+
+	data, err := x509.CreateCertificate(rand.Reader, &template, &template, &rkey.PublicKey, rkey)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pub := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: data})
+	key := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rkey)})
+
+	return pub, key, nil
+}
+
+func stream(a, b net.Conn) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go copyWait(a, b, &wg)
+	go copyWait(b, a, &wg)
+	wg.Wait()
+	a.Close()
+	b.Close()
+}
+
+func copyWait(w io.Writer, r io.Reader, wg *sync.WaitGroup) {
+	defer wg.Done()
+	io.Copy(w, r)
+}
